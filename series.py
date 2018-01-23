82907ee08ace76284c50b26e1981cccf1e47d49a
CharlyWelch <blwbiology@gmail.com>
2018-01-23 15:46:33 -0800

    add lucas sequence function and tests for lucas

--- series.py
+++ series.py
@@ -6,10 +6,16 @@ def fibonacci(n):
         return 1
     return fibonacci(n-1) + fibonacci(n-2)
 
+
+def lucas(n):
+    """ Recursive solution to return a lucas sequence """
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    return lucas(n-1) + lucas(n-2)
+
 print("Start")
 if __name__ == "__main__":
     print(fibonacci(3))
-
-# for number in range(0,10):
-#     print (fibonacci(number), end = ' ')
-
+    print(lucas(2))
--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-from series import fibonacci
+from series import fibonacci, lucas
 """ Importing the object fibonacci, which is a function defined in series.py"""
 
 import unittest
@@ -9,6 +9,13 @@ class TestIt(unittest.TestCase):
 
     def test_2(self):
         self.assertEqual(fibonacci(4), 2)
+
+class TestIt2(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(lucas(3), 4)
+
+    def test_2(self):
+        self.assertEqual(lucas(4), 3)
 
 if __name__ == '__main__':
     unittest.main()
