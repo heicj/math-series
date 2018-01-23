0d409d45ba2a9fc11fe31390934af80ac9f2caf2
CharlyWelch <blwbiology@gmail.com>
2018-01-23 15:20:28 -0800

    add functions for fib. sequence and testing

--- series.py
+++ series.py
@@ -1,6 +1,15 @@
-
 def fibonacci(n):
-    """  """
+    """ Recursive solution to return a fibonacci sequence """
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
-def lucas(n):
-    """  """
+print("Start")
+if __name__ == "__main__":
+    print(fibonacci(3))
+
+# for number in range(0,10):
+#     print (fibonacci(number), end = ' ')
+
--- test_series.py
+++ test_series.py
@@ -1,1 +1,14 @@
+from series import fibonacci
+""" Importing the object fibonacci, which is a function defined in series.py"""
 
+import unittest
+
+class TestIt(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(fibonacci(3), 2)
+
+    def test_2(self):
+        self.assertEqual(fibonacci(4), 2)
+
+if __name__ == '__main__':
+    unittest.main()
