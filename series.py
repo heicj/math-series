f9e44102318d1d88c5b7f916379ed99e26b0bab6
Charlie Heiner <heinercj@gmail.com>
2018-01-23 16:01:49 -0800

    fibonnacci function lucas number function

--- series.py
+++ series.py
@@ -1,1 +1,29 @@
+def fibonnacci(n):
+	""" recursive solution to fibonnacci"""
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	
+	return fibonnacci(n-2) + fibonnacci(n-1)
 
+def lucas(n):
+	""" 2 if n == 0
+		1 if n == 1
+		L n-1 + L n-2 if n > 1
+	"""
+	if n == 0:
+		return 2
+	if n == 1:
+		return 1
+	
+	return lucas(n - 1) + lucas(n - 2)
+	
+	
+
+if __name__ == "__main__":
+	print (fibonnacci(3))
+	print (lucas(2))
+	
+
+	
--- test_series.py
+++ test_series.py
@@ -1,1 +1,18 @@
+from series import fibonnacci
+from series import lucas
+import unittest
 
+class TestIt(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual(fibonnacci(3), 2)
+	def test_2(self):
+		self.assertEqual(fibonnacci(4), 2)
+	def test_3(self):
+		self.assertEqual(lucas(2), 3)
+	def test_4(self):
+		self.assertEqual(lucas(4), 6)
+
+	
+if __name__ == '__main__':
+	unittest.main()
+	
